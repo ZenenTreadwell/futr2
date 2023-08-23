2bf6f6931e42679dda4d0e955ff39f6859f3e64f
Your Name <you@example.com>
2023-08-23 16:55:27 -0700

    m

--- src/Nostr/Beam.hs
+++ src/Nostr/Beam.hs
@@ -100,7 +100,7 @@ instance Table PlebT where
 data ReplyT f = Reply {
         _idxr :: C f Int32
       , _eidr :: PrimaryKey EvT f
-      , _eidrr :: PrimaryKey EvT f
+      , _eidrr :: (C f Text) -- Either (PrimaryKey EvT f) 
       , _markerr :: C f (Maybe Marker)
       } deriving (Generic, Beamable) 
 type Reply = ReplyT
@@ -110,15 +110,15 @@ instance Table ReplyT where
       primaryKey = ReplyId . _idxr
 
 data MentionT f = Mention {
-        _idxM :: C f Int32
-      , _eidM :: PrimaryKey EvT f
-      , _pidM :: PrimaryKey PlebT f
+        _idxm :: C f Int32
+      , _eidm :: PrimaryKey EvT f
+      , _pidm :: C f Text -- PrimaryKey PlebT f
       } deriving (Generic, Beamable)
 type Mention = MentionT
 type MentionId = PrimaryKey MentionT Identity
 instance Table MentionT where 
       data PrimaryKey MentionT f = MentionId (C f Int32) deriving (Generic, Beamable)
-      primaryKey = MentionId . _idxM
+      primaryKey = MentionId . _idxm
 
 instance HasDefaultSqlDataType Sqlite Marker where 
     defaultSqlDataType _ _ _ = sqliteTextType 
@@ -130,52 +130,21 @@ instance FromBackendRow Sqlite Marker where
 insertEv :: Connection -> Event -> IO ()
 insertEv conn e@(Event i s (Content{..})) = -- do 
     -- ins <- E.try 
-    (runBeamSqlite conn $ do
+    runBeamSqliteDebug print conn $ do
+
+        -- XXX run as a withTransaction??
 
         runInsert $ insertOnConflict (_plebs spec') (insertValues [Pleb . wq $ pubkey]) anyConflict onConflictDoNothing
         
         runInsert $ insert (_events spec') (insertValues [toEv e])
-        
-        let (m', r') = fromTags i tags
-        
-        runInsert $ insert (_mentions spec') $ insertExpressions $ flip map m' \(Mention _ b c)-> 
-            Mention default_ (val_ b) (val_ c)      
-        runInsert $ insert (_replies spec') $ insertExpressions $ flip map r' \(Reply _ e r m)->
-            Reply default_ (val_ e) (val_ r) (val_ m) 
-        
-        ) -- ??? :: Exception e => IO (Either e ())
 
-    -- case ins of 
-    --     Right r -> print "insert right" >> print r  
-    --     Left _ -> print "insert left " 
-
-type TagS = ([MentionT Identity], [ReplyT Identity])
-
--- fromTags :: [Tag] -> TagS 
-fromTags eid tags = evalState runTags (tags, ([], []))
-    where 
-
-    runTags :: State ([Tag], TagS) TagS
-       
-    runTags = do 
-        ( tx , b@(m', r') ) <- get
-        case tx of 
-            [] -> pure b 
-            t' : tx' -> do 
-              case t' of
-                ETag idx _ marker -> 
-                    put ( tx' , ( 
-                      m' 
-                    , Reply 1 (EvId . wq $ eid) (EvId . wq $ idx) marker : r'
-                    ))
-                PTag idx _ -> 
-                    put (tx', (
-                        Mention 1 (EvId . wq $ eid) (PlebId . wq $ idx) : m'
-                      , r'
-                      ))
-                Tag _ -> put (tx', b) 
-              runTags
-
+        mconcat <$> flip mapM tags \case
+            ETag id _ marker -> runInsert $ insert (_replies spec') $ insertExpressions $ [
+                  Reply default_ (val_ . EvId . wq $ i) (val_ . wq $ id) (val_ marker)
+                ]
+            PTag id _ -> runInsert $ insert (_mentions spec') $ insertExpressions $ [
+                  Mention default_ (val_ . EvId . wq $ i) (val_ . wq $ id) ]
+            _ -> pure () 
 
 insertId :: Connection -> Text -> IO ()
 insertId conn privKey = runBeamSqlite conn $ do
